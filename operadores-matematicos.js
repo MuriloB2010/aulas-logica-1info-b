db2bad997194b32745725c77fd9bce68bf805273
murilo b <cristinabritto@gmail.com>
2026-03-19 16:44:53 -0300

    aulas-logica-1info-b

---
+++ operadores-matematicos.js
@@ -0,0 +1,12 @@
+//comentário de linha
+
+/*
+aqui vai comentários
+com mais de 
+uma linha
+*/
+
+//+, -, *, /, % resto da divisão, ** exponenciação
+// + adição/ concatenação
+
+console.log (5 != 5)
